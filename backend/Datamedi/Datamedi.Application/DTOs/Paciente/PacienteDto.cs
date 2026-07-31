37dd07851cd4a82ba9550aae9a511343d26f266e
Anderson Martinez <anphimarmu@gmail.com>
2026-07-31 18:52:53 -0500

    creacion de clases dentro de DTO paciente

---
+++ backend/Datamedi/Datamedi.Application/DTOs/Paciente/PacienteDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datamedi.Application.DTOs.Paciente
+{
+    //recibir datos de un paciente para mostrar en el sistema
+    public class PacienteDto
+    {// id del paciente
+        public int Id { get; set; } // Cédula o identificador
+        // nombre del paciente
+        public string Nombre { get; set; } = string.Empty;
+        // apellido del paciente
+        public string Apellido { get; set; } = string.Empty;
+        //fecha de nacimiento del paciente
+        public DateTime FechaNacimiento { get; set; }
+        // sexo del paciente
+        public string Genero { get; set; } = string.Empty;
+        // telefono del paciente
+        public string Telefono { get; set; } = string.Empty;
+        // direccion del paciente
+        public string Direccion { get; set; } = string.Empty;
+        // correo del paciente
+        public string Correo { get; set; } = string.Empty;
+    }
+}
